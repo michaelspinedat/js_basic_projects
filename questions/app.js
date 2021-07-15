75df634e9412204636e879779b89db47043881cd
michael <stevenpnd@gmail.com>
2021-07-15 17:52:32 -0500

    Show answers to questions
    Using selectors inside the questions elements

--- questions/app.js
+++ questions/app.js
@@ -1,8 +1,21 @@
-const btns = document.querySelectorAll('.question-btn');
+// traversing the dom
 
-btns.forEach((btn) => {
-    btn.addEventListener('click', e => {
-        const question = e.currentTarget.parentElement.parentElement;
+// const btns = document.querySelectorAll('.question-btn');
+
+// btns.forEach((btn) => {
+//     btn.addEventListener('click', e => {
+//         const question = e.currentTarget.parentElement.parentElement;
+//         question.classList.toggle('show-text');
+//     });
+// });
+
+// using selectors inside the element
+
+const questions = document.querySelectorAll('.question');
+
+questions.forEach(question => {
+    const btn = question.querySelector('.question-btn');
+    btn.addEventListener('click', () => {
         question.classList.toggle('show-text');
     });
 });
