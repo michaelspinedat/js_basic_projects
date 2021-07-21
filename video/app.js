565ec1744afc7a439a804e68173951d285858898
michael <stevenpnd@gmail.com>
2021-07-21 17:46:30 -0500

    Add preloader

--- video/app.js
+++ video/app.js
@@ -1,6 +1,13 @@
 const btn = document.querySelector('.switch-btn');
 const video = document.querySelector('.video-container');
 
+// preloader
+const preloader = document.querySelector('.preloader');
+
+window.addEventListener('load', () => {
+    preloader.classList.add('hide-preloader');
+});
+
 btn.addEventListener('click', () => {
     if(!btn.classList.contains('slide')) {
         btn.classList.add('slide');
